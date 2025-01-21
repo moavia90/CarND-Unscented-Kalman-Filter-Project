47d4f918edef9634e6ad47be164dac3b20472901
moavia90 <moaviaadeel@gmail.com>
2025-01-21 19:37:23 +0500

    Implemented Function for calculating Root Mean Squared Error (RMSE)

--- src/tools.cpp
+++ src/tools.cpp
@@ -9,7 +9,18 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-   * TODO: Calculate the RMSE here.
-   */
+                                 
+   VectorXd rmse(4);
+	rmse << 0,0,0,0;
+   VectorXd res(4);
+
+   for(int i=0; i < estimations.size(); ++i){
+      res = estimations[i] - ground_truth[i];
+      res = res.array() * res.array();
+      rmse += res;
+   }
+
+   rmse /=  estimations.size();
+   rmse = rmse.array().sqrt();
+   return rmse;
 }
